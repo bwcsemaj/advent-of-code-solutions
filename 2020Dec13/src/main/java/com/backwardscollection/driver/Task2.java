e84d9eff66c013b7213959a325452d6de6c60af3
bwcsemaj <bwc_semaj@live.com>
2020-12-13 05:47:47 -0500

    initializing task 2

---
+++ 2020Dec13/src/main/java/com/backwardscollection/driver/Task2.java
@@ -0,0 +1,23 @@
+package com.backwardscollection.driver;
+
+public class Task2 {
+    
+    public static void main(String[] args) {
+        // Obtain Input
+        
+        // Convert input into Model
+        
+        // Organize ids by highest to lowest
+        
+        // Calculate earliest time when all bus ids
+        // correlate to the index they are in regarding timestamp
+        
+        // Display result
+        
+        
+    }
+    
+    private void calculateEarliestTime(){
+    
+    }
+}
